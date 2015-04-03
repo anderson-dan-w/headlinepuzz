4c8b96590a899780fce31feb3ad6f7aa804627bc
dwanderson <anderson.dan.w@gmail.com>
2015-04-02 20:15:06 -0400

    word unittests. currently failing

---
+++ tests/test_word.py
@@ -0,0 +1,88 @@
+#!/usr/bin/python3
+import os
+import json
+import unittest
+
+from .. import word as hlpword
+
+HERE = os.path.dirname(os.path.realpath(__file__))
+
+class PatternMapperTestCase(unittest.TestCase):
+    def setUp(self):
+        self.pattern_fname = os.path.join(HERE, "patterns.json")
+        self.dict_fnames = [os.path.join(HERE, "words.dict"),
+                            os.path.join(HERE, "more_words.dict")]
+        self.cipher_fname = os.path.join(HERE, "cipher_words.json")
+        with open(self.pattern_fname) as fh:
+            self.expected_patterns = json.load(fh)
+        self.expected_words = {word for words in self.expected_patterns.values()
+                                    for word in words}
+        with open(self.cipher_fname) as fh:
+            self.cipher_words = json.load(fh)
+        ## json doesn't like python sets; override it
+        for d in self.cipher_words.values():
+            d['words'] = set(d['words'])
+        ## for premade PatternMapper, when we aren't testing the init
+        self.pm = hlpword.PatternMapper(pattern_fname=self.pattern_fname)
+
+    def test_readin_pattern_file(self):
+        pm = hlpword.PatternMapper(pattern_fname=self.pattern_fname)
+        self.assertEqual(self.expected_patterns, pm.patterns,
+                "expected {}; got {}"
+                .format(self.expected_patterns, pm.patterns))
+        self.assertEqual(self.expected_words, pm.words,
+                "expected {}; got {}".format(self.expected_words, pm.words))
+
+    def test_readin_dict_files(self):
+        pm = hlpword.PatternMapper(pattern_fname="nonexistent_file",
+                                   dict_fnames=self.dict_fnames)
+        self.assertEqual(self.expected_words, pm.words,
+                "expected {}; got {}".format(self.expected_words, pm.words))
+
+    def test_save_pattern_file(self):
+        test_fname = "test_save.json"
+        try:
+            self.pm.save_pattern_file(test_fname, HERE)
+            ## strip newlines because json.dump() has noeol at eof
+            ## but manually created files implicitly do
+            with open(os.path.join(HERE, self.pattern_fname)) as fh:
+                expected_text = fh.read().replace("\n","")
+            with open(os.path.join(HERE, test_fname)) as fh:
+                text = fh.read().replace("\n","")
+            self.assertEqual(expected_text, text,
+                    "expected {}; got {}".format(expected_text, text))
+        finally:
+            if os.path.exists(os.path.join(HERE, test_fname)):
+                os.remove(os.path.join(HERE, test_fname))
+
+    def test_get_pattern(self):
+        for word in self.expected_words:
+            pattern = self.pm.get_pattern(word)
+            self.assertIn(pattern, self.expected_patterns,
+                    "didn't find expected pattern {}".format(pattern))
+            self.assertIn(word, self.pm.patterns[pattern],
+                    "word {} didn't show up under pattern {} as expected"
+                    .format(word, pattern))
+
+    def test_find_words(self):
+        for cipher_word, values in self.cipher_words.items():
+            words = self.pm.find_words(cipher_word)
+            expected_words = values['words']
+            self.assertEqual(expected_words, words,
+                    "expected {}; got {}".format(expected_words, words))
+
+    def test_find_words_with_mapping(self):
+        for cipher_word, values in self.cipher_words.items():
+            maps = {'all': len(values['words']), 'one':1, 'none':0}
+            for map_type, count in maps.items():
+                mapping = values['mapping'][map_type]
+                words = self.pm.find_words(cipher_word, mapping=mapping)
+                self.assertEqual(count, len(words),
+                        "expected {} words; got {}".format(count, len(words)))
+
+class WordTestCase(unittest.TestCase):
+    ''' Tests for headlinepuzz.word '''
+    pass
+
+if __name__ == '__main__':
+    unittest.main()
