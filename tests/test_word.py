22e0042f31db5874443d376dff0302ab0df1b05a
dwanderson <anderson.dan.w@gmail.com>
2015-04-02 21:50:39 -0400

    finished tests; all pass
    code coverage is high but not 100%; the 'untested' lines are those which
    refer to defaults that are 'production' defaults, and so I don't want to use
    them during tests. Not sure if there's a more reasonable way around that

--- tests/test_word.py
+++ tests/test_word.py
@@ -82,7 +82,50 @@ class PatternMapperTestCase(unittest.TestCase):
 
 class WordTestCase(unittest.TestCase):
     ''' Tests for headlinepuzz.word '''
-    pass
+    def setUp(self):
+        self.pattern_fname = os.path.join(HERE, "patterns.json")
+        self.cipher_fname = os.path.join(HERE, "cipher_words.json")
+        with open(self.cipher_fname) as fh:
+            self.cipher_words = json.load(fh)
+        ## json doesn't like python sets; override it
+        for d in self.cipher_words.values():
+            d['words'] = set(d['words'])
+        ## override default Word.PATTERN_MAPPER; work with smaller dicts, etc
+        self.pm = hlpword.PatternMapper(pattern_fname=self.pattern_fname)
+        hlpword.Word.PATTERN_MAPPER = self.pm
+
+    def test_init(self):
+        for cipher_word, values in self.cipher_words.items():
+            word = hlpword.Word(cipher_word)
+            self.assertEqual(cipher_word, word.original_word)
+            self.assertEqual(cipher_word, word.word)
+            self.assertEqual(values['pattern'], word.pattern)
+
+    def test_plain(self):
+        word = hlpword.Word("DOGS")
+        self.assertEqual(self.pm.DIT*len(word.word), word.plain)
+        word.plain = "F"
+        self.assertEqual("F"+self.pm.DIT*(len(word.word) - 1), word.plain)
+        word.plain = "FISH"
+        self.assertEqual("FISH", word.plain)
+
+    def test_is_fully_set(self):
+        word = hlpword.Word("DOGS")
+        self.assertFalse(word.is_fully_set)
+        word.plain = "F"
+        self.assertFalse(word.is_fully_set)
+        word.plain = "FISH"
+        self.assertTrue(word.is_fully_set)
+
+    def test_possibles(self):
+        word = hlpword.Word("BUBBA")
+        self.assertEqual({'SASSY', 'NANNY'}, word.possibles)
+        word.plain = "P"
+        self.assertEqual(set(), word.possibles)
+        word.plain = ""
+        self.assertEqual({'SASSY', 'NANNY'}, word.possibles)
+        word.plain = "S"
+        self.assertEqual({'SASSY'}, word.possibles)
 
 if __name__ == '__main__':
     unittest.main()
